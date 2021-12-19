22a7c356a2f29d2f3aeaa3c958deda4d9f66398b
Gustavo Sampaio <37808461+gstsdev@users.noreply.github.com>
2021-12-18 22:38:38 -0300

    Resetting config

--- config.php
+++ config.php
@@ -8,24 +8,21 @@ define('DB_SERVER', 'localhost');
 
 define('DB_USERNAME', 'root');
 
-define('DB_PASSWORD', 'aluno');
+define('DB_PASSWORD', '');
 
 define('DB_NAME', 'ifome');
 
- 
+
 
 /* Attempt to connect to MySQL database */
 
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
- 
+
 
 // Check connection
 
-if($mysqli === false){
+if ($mysqli->connect_error) {
 
     die("ERROR: Could not connect. " . $mysqli->connect_error);
-
 }
-
-?>
